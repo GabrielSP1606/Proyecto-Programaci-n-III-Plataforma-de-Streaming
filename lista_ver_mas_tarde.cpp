36563ae70ddc73899d86e01c4978d766978f1290
Gabriel Saavedra <gabrielsaavedra@MacBook-Pro-de-Gabriel.local>
2026-05-03 12:15:44 -0500

    persona 5: lista ver mas tarde

---
+++ lista_ver_mas_tarde.cpp
@@ -0,0 +1,62 @@
+#include "lista_ver_mas_tarde.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> listaVerMasTarde;
+vector<int> listaLikes;
+
+void cargarListas() {
+    ifstream archivoVer("ver_mas_tarde.txt");
+    int id;
+    while (archivoVer >> id) {
+        listaVerMasTarde.push_back(id);
+    }
+    archivoVer.close();
+
+    ifstream archivoLikes("likes.txt");
+    while (archivoLikes >> id) {
+        listaLikes.push_back(id);
+    }
+    archivoLikes.close();
+}
+
+void guardarListas() {
+    ofstream archivoVer("ver_mas_tarde.txt");
+    for (int id : listaVerMasTarde) {
+        archivoVer << id << "\n";
+    }
+    archivoVer.close();
+
+    ofstream archivoLikes("likes.txt");
+    for (int id : listaLikes) {
+        archivoLikes << id << "\n";
+    }
+    archivoLikes.close();
+}
+
+void agregarVerMasTarde(int idPelicula) {
+    listaVerMasTarde.push_back(idPelicula);
+    guardarListas();
+}
+
+void agregarLike(int idPelicula) {
+    listaLikes.push_back(idPelicula);
+    guardarListas();
+}
+
+void mostrarVerMasTarde(const vector<Pelicula>& peliculas) {
+    if (listaVerMasTarde.empty()) {
+        cout << "Tu lista de Ver mas tarde esta vacia.\n";
+        return;
+    }
+    cout << "=== Ver mas tarde ===\n";
+    for (int id : listaVerMasTarde) {
+        if (id >= 0 && id < peliculas.size()) {
+            cout << "- " << peliculas[id].titulo
+                      << " (" << peliculas[id].releaseYear << ") "
+                      << "[" << peliculas[id].genero << "]\n";
+        }
+    }
+}
--- lista_ver_mas_tarde.h
+++ lista_ver_mas_tarde.h
@@ -1,8 +1,18 @@
-//
-// Created by Gabriel Saavedra on 2/05/26.
-//
+#ifndef LISTA_VER_MAS_TARDE_H
+#define LISTA_VER_MAS_TARDE_H
 
-#ifndef PROYECTO_PROGRA_3_LISTA_VER_MAS_TARDE_H
-#define PROYECTO_PROGRA_3_LISTA_VER_MAS_TARDE_H
+#include <vector>
+#include <string>
+#include "pelicula.h"
+using namespace std;
 
-#endif //PROYECTO_PROGRA_3_LISTA_VER_MAS_TARDE_H
+vector<int> listaVerMasTarde;
+vector<int> listaLikes;
+
+void agregarVerMasTarde(int idPelicula);
+void agregarLike(int idPelicula);
+void cargarListas();
+void guardarListas();
+void mostrarVerMasTarde(const vector<Pelicula>& peliculas);
+
+#endif
