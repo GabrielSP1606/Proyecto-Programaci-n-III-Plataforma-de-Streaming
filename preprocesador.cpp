8615bfcf98c3d20ec101abe97627bc7c10767102
e-mendoza2007 <ernesto.mendoza@utec.edu.pe>
2026-05-03 14:32:33 +0000

    Stop Words

--- preprocesador.cpp
+++ preprocesador.cpp
@@ -1,5 +1,30 @@
 #include "preprocesador.h"
+#include <string>
+#include <vector>
+#include <unordered_set>
 
+using namespace std;
+
+unordered_set<string> stopwords = {
+    "i","me","my","myself","we","our","ours","ourselves",
+    "you","your","yours","yourself","yourselves",
+    "he","him","his","himself","she","her","hers","herself",
+    "it","its","itself","they","them","their","theirs","themselves",
+    "what","which","who","whom","this","that","these","those",
+    "am","is","are","was","were","be","been","being",
+    "have","has","had","having",
+    "do","does","did","doing",
+    "a","an","the",
+    "and","but","if","or","because","as","until","while",
+    "of","at","by","for","with","about","against","between",
+    "into","through","during","before","after","above","below",
+    "to","from","up","down","in","out","on","off","over","under",
+    "again","further","then","once",
+    "here","there","when","where","why","how",
+    "all","any","both","each","few","more","most","other",
+    "some","such","no","nor","not","only","own","same",
+    "so","than","too","very","can","will","just","should"
+};
 
 string normalizar(string texto) {
     string r = "";
@@ -7,11 +32,9 @@ string normalizar(string texto) {
     for (int i = 0; i < texto.size(); i++) {
         char c = texto[i];
 
-
         if (c >= 'A' && c <= 'Z') {
             c = c + 32;
         }
-
 
         if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == ' ') {
             r += c;
@@ -27,17 +50,23 @@ vector<string> tokenizar(string texto) {
 
     for (int i = 0; i < texto.size(); i++) {
         if (texto[i] == ' ') {
+
             if (actual != "") {
-                palabras.push_back(actual);
+                if (stopwords.count(actual) == 0) {
+                    palabras.push_back(actual);
+                }
                 actual = "";
             }
+
         } else {
             actual += texto[i];
         }
     }
 
     if (actual != "") {
-        palabras.push_back(actual);
+        if (stopwords.count(actual) == 0) {
+            palabras.push_back(actual);
+        }
     }
 
     return palabras;
