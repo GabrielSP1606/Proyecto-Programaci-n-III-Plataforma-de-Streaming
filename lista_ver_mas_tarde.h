c93d7577b6e9b9dec53fe485b1b6a0ab073abf60
Gabriel Saavedra <gabrielsaavedra@MacBook-Pro-de-Gabriel.local>
2026-05-03 12:20:42 -0500

    persona 5: lista ver mas tarde completa

--- lista_ver_mas_tarde.h
+++ lista_ver_mas_tarde.h
@@ -2,17 +2,12 @@
 #define LISTA_VER_MAS_TARDE_H
 
 #include <vector>
-#include <string>
 #include "pelicula.h"
-using namespace std;
-
-vector<int> listaVerMasTarde;
-vector<int> listaLikes;
 
 void agregarVerMasTarde(int idPelicula);
 void agregarLike(int idPelicula);
 void cargarListas();
 void guardarListas();
-void mostrarVerMasTarde(const vector<Pelicula>& peliculas);
+void mostrarVerMasTarde(const std::vector<Pelicula>& peliculas);
 
 #endif
